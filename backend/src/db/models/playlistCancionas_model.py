f31b6e2b3b605c6d9a0a66172cf564d94b9cae13
flidoscordo <guido.scordo@philips.edu.ar>
2026-05-15 10:35:49 -0300

    subo playlist canciones model

---
+++ backend/src/db/models/playlistCancionas_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+
+from src.db.connection import Base
+
+
+class Playlist_canciones(Base):
+    __tablename__ = "Playlist_canciones"
+
+    playlist_id = Column(Integer, nullable=False)
+    cancion_id = Column(String, nullable=False)
+    orden = Column(Integer, nullable=False)
+    fecha_agregada = Column(DateTime)
