a427b612a23e1e4815f7a6aecc4054fa9eb9c1c6
nixosnazer <nicolas.nazer@philips.edu.ar>
2026-05-15 11:59:03 -0300

    arreglando errores

--- backend/src/db/models/favoritos_model.py
+++ backend/src/db/models/favoritos_model.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
-from songs_model import Canciones
+
 from src.db.connection import Base
 
 
-class Favoritos(Canciones):
+class Favoritos(Base):
     __tablename__ = "Favoritos"
 
     usuario_id = Column(Integer, primary_key=True, notnull=True)
--- backend/src/db/models/seguidores_model.py
+++ backend/src/db/models/seguidores_model.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
-from user_model import User
 from src.db.connection import Base
 
 
-class Seguidores(User):
+class Seguidores(Base):
     __tablename__ = "seguidores"
 
     usuario_id = Column(Integer, nullable=False)
