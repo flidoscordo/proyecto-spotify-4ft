2bfd01aa7e0180de8982dcc995035dc1784b46ef
nixosnazer <nicolas.nazer@philips.edu.ar>
2026-05-15 10:36:52 -0300

    model seguidores

---
+++ backend/src/db/models/seguidores_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+
+from src.db.connection import Base
+
+
+class Seguidores(Base):
+    __tablename__ = "seguidores"
+
+    usuario_id = Column(Integer, nullable=False)
+    artista_id = Column(Integer, nullable=False)
+    fecha_seguimiento = Column(DateTime)
