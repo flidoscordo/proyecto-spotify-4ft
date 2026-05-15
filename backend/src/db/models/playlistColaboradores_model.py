64b99f2f6abe830aa958f8d39eb6df09a4b84330
nixosnazer <nicolas.nazer@philips.edu.ar>
2026-05-15 11:52:41 -0300

    acutalizacion colaboradores

--- backend/src/db/models/playlistColaboradores_model.py
+++ backend/src/db/models/playlistColaboradores_model.py
@@ -1,8 +1,11 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from src.db.connection import Base
+from playlists_model import Playlists
 
-class playlist_colaboradores(Base):
+class playlist_colaboradores(Playlists):
+    __tablename__ = "playlist_colaboradores"
+    
     playlist_id = Column(Integer,nullable=False)
     usuario_id = Column(Integer, nullable = False)
     fecha_agregado = Column(DateTime)
